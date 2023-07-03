c22d801bcfe93fca20f5931bea7ee7545b7786fe
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-07-03 11:47:47 +0530

    update

---
+++ Index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("hello Kalpesh")
+})
+
+app.get("/one", (req, res) => {
+    res.send("hello Guys yaha party ho rahi hai")
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+})

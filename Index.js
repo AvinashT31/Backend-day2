f612fb0caa6105b92aec2778ac845f72d6e40908
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-07-03 11:57:09 +0530

    github

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("hello Kalpesh")
+    res.send("hello Avinash");
 })
 
 app.get("/one", (req, res) => {
